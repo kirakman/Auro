1838599e01a587370c30ba2a1d6b24f580d9ab44
kirakman <dourado178@gmail.com>
2024-12-30 19:56:39 -0300

    Primeiro commit

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const headerContainer = document.getElementById('header');
+
+    fetch('components/header.html')
+        .then(response => {
+            if (!response.ok) throw new Error('Failed to fetch header');
+            return response.text();
+        })
+        .then(data => {
+            headerContainer.innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading header:', error);
+        });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const container1Container = document.getElementById('container1');
+
+    fetch('components/container1.html')
+        .then(response => {
+            if (!response.ok) throw new Error('Failed to fetch header');
+            return response.text();
+        })
+        .then(data => {
+            container1Container.innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading header:', error);
+        });
+});
+
+

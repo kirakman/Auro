196ba61d73e6f701a20c28344d5b2cf66e41c7ce
kirakman <dourado178@gmail.com>
2024-12-30 20:08:15 -0300

    Criando container2

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,4 +30,20 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    const container2Container = document.getElementById('container2');
+
+    fetch('components/container2.html')
+        .then(response => {
+            if (!response.ok) throw new Error('Failed to fetch header');
+            return response.text();
+        })
+        .then(data => {
+            container2Container.innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading header:', error);
+        });
+});
+
 
